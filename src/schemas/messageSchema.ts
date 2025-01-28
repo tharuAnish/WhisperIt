866b33beb9afa61018fc3f9038e6af01255ffc59
tharuAnish <namoanishtharu@gmail.com>
2025-01-28 08:17:57 +0530

    Created schemas for signin signup message

--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -3,6 +3,6 @@ import { z } from "zod"
 export const messageSchema = z.object({
   content: z
     .string()
-    .min(10, { message: "Content must be at least 10 characters." })
+    .min(4, { message: "Content must be at least 4 characters." })
     .max(300, { message: "Content must not be longer than 300 characters." }),
 })
